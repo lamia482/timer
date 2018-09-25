c2ecefd081e3a21f303dbe525c2ff5fe79580af8
lamia482 <wangqiang482@icloud.com>
2018-09-25 14:39:57 +0800

    Update main.cc

--- main.cc
+++ main.cc
@@ -12,16 +12,17 @@
 auto timer = new lamia::LamiaTimer();
 typedef size_t DTYPE;
 static const DTYPE MAX_MOD_SIZE = 10000000007;
-static const DTYPE MAX_SAVED_FACTORY_SIZE = 0;
+static const DTYPE MAX_SAVED_FACTORY_SIZE = 1;
 
 DTYPE factory(DTYPE x, DTYPE MOD = MAX_MOD_SIZE) {
   if(x < 0) return 0;
   static std::map<DTYPE, DTYPE> factory_map;
   factory_map[0] = 1; factory_map[1] = 1;
-  if(x < MAX_SAVED_FACTORY_SIZE) return factory_map[x];
-  for(DTYPE i = MAX_SAVED_FACTORY_SIZE > 1? MAX_SAVED_FACTORY_SIZE: 1; i <= x; ++i) {
+  if(x <= MAX_SAVED_FACTORY_SIZE) return factory_map[x];
+  for(DTYPE i = MAX_SAVED_FACTORY_SIZE + 1; i <= x; ++i) {
     factory_map[i] = i * factory_map[i - 1] & MAX_MOD_SIZE;
   }
+  MAX_SAVED_FACTORY_SIZE = x;
   return factory_map[x];
 }
 
